3e6cda1dafb95ec29c35565daa1731a4d57f8dde
GiovanniMarseglia <giovannimarseglia00@gmail.com>
2024-05-16 14:36:27 +0200

    Completed third snack

---
+++ src/org/learning/SumEven.java
@@ -0,0 +1,22 @@
+package org.learning;
+
+import java.util.Scanner;
+
+public class SumEven {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum=0,count;
+        count = scanner.nextInt();
+        int[] listNumber = new int[count];
+
+        for (int i = 0; i < count; i++) {
+            listNumber[i] = (int)(Math.random()*100)+1;
+            System.out.println("Numero alla posizione "+ i + " " + listNumber[i]);
+            if(i % 2 != 0){
+                sum += listNumber[i];
+            }
+        }
+
+        System.out.println("La somma dei valori in pèosizione dispari è di: " + sum);
+    }
+}
