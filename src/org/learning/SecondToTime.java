243c7b363541fc39364fed76e784ae4aa0d5989f
GiovanniMarseglia <giovannimarseglia00@gmail.com>
2024-05-16 16:43:10 +0200

    Complited seventh Snack

---
+++ src/org/learning/SecondToTime.java
@@ -0,0 +1,23 @@
+package org.learning;
+
+import java.util.Scanner;
+
+public class SecondToTime {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Inserisci i secondi");
+        int inputSecond = scanner.nextInt();
+        int second, minute=0, hour=0;
+        if(inputSecond > 59){
+            minute = inputSecond / 60;
+            second = inputSecond % 60;
+            if (minute>59){
+                hour = minute / 60;
+                minute = minute % 60;
+            }
+        }else{
+            second = inputSecond;
+        }
+        System.out.println(hour + ":" + minute + ":" + second);
+    }
+}
